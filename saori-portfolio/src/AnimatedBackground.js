4712e12a9ddfdc0bb6d930226c8938ef0d6978c9
Saori Uchida <sebauchida@gmail.com>
2023-10-30 23:27:47 -0400

    filter in bg

--- saori-portfolio/src/AnimatedBackground.js
+++ saori-portfolio/src/AnimatedBackground.js
@@ -7,30 +7,36 @@ function getRandomColor() {
 }
 
 function getRandomPosition() {
-    return {
-      left: `${Math.random() * 100}vw`,
-      top: `${Math.random() * 100}vh`
-    };
-  }
-  
-  function AnimatedBackground() {
-    return (
-      <div>
-        {colors.map((_, index) => (
+  return {
+    left: `${Math.random() * 100}vw`,
+    top: `${Math.random() * 100}vh`
+  };
+}
+
+function getRandomWidth() {
+  // Randomize width between 50% to 100%
+  return `${50 + Math.random() * 50}vw`;
+}
+
+function AnimatedBackground() {
+  return (
+    <div>
+      {colors.map((_, index) => {
+        const randomColor = getRandomColor();
+        return (
           <div 
             key={index} 
             className="wedge" 
             style={{ 
               ...getRandomPosition(),
-              backgroundColor: getRandomColor(), 
-              width: '100px', 
-              height: '100px' 
+              width: getRandomWidth(),
+              boxShadow: `rgb(250, 250, 250) 0px 0px 100px -40px inset, ${randomColor} 0px 0px 30vw 30vw inset, ${randomColor} 0px 0px 70px 40px`
             }} 
           />
-        ))}
-      </div>
-    );
-  }
-  
+        );
+      })}
+    </div>
+  );
+}
 
 export default AnimatedBackground;
