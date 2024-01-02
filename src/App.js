acbff11964da3ee62a5adc09f05a6520477721a3
Saori Uchida <sebauchida@gmail.com>
2024-01-02 18:17:27 -0300

    Add react-gtm-module dependency and Google Tag Manager script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { track } from '@vercel/analytics';
 import './App.css';
 import Modal from 'react-modal';
 import Draggable from 'react-draggable';
@@ -132,17 +131,17 @@ function App() {
       <AnimatedBackground isLoading={isLoading} />
       <div className='title'>
         <div className='contact'>
-          <a href="https://linkedin.com/in/saoriuchida/" target="_blank" rel="noopener noreferrer" onClick={() => track('LinkedIn Click')}>
+          <a href="https://linkedin.com/in/saoriuchida/" target="_blank" rel="noopener noreferrer" onClick={() => window.gtag('event', 'click', { 'event_category': 'link', 'event_label': 'LinkedIn Click' })}>
             <div className="logo-container">
               <img src={LinkedInLogo} alt="LinkedIn" className="linkedin-logo" />
             </div>
           </a>
-          <a href="https://github.com/saoriu" target="_blank" rel="noopener noreferrer" onClick={() => track('Git Click')}>
+          <a href="https://github.com/saoriu" target="_blank" rel="noopener noreferrer" onClick={() => window.gtag('event', 'click', { 'event_category': 'link', 'event_label': 'Git Click' })}>
             <div className="logo-container">
               <svg className="github-logo" viewBox="0 0 100 100" width="30" height="30" xmlns="http://www.w3.org/2000/svg"><path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" /></svg>
             </div>
           </a>
-          <a href="mailto:saori.uchid@gmail.com" target="_blank" rel="noopener noreferrer" onClick={() => track('Email Click')}>
+          <a href="mailto:saori.uchid@gmail.com" target="_blank" rel="noopener noreferrer" onClick={() => window.gtag('event', 'click', { 'event_category': 'link', 'event_label': 'Email Click' })}>
             <div className="logo-container">
               <svg fill="white" version="1.1" id="Capa_1" viewBox="0 0 216 216" className="email-logo">
                 <path d="M108,0C48.353,0,0,48.353,0,108s48.353,108,108,108s108-48.353,108-108S167.647,0,108,0z M156.657,60L107.96,98.498
@@ -154,7 +153,7 @@ function App() {
         </div>
         <button className={`ai-button ${isCollapsed ? 'collapsed' : 'expanded'}`} onClick={() => {
           setIsCollapsed(!isCollapsed);
-          track('AI Button Click', { isCollapsed: !isCollapsed });
+          window.gtag('event', 'click', { 'event_category': 'button', 'event_label': 'AI Button Click', 'value': !isCollapsed });
         }}>
           {isCollapsed ? 'ask my ai' : 'ask my ai'}
         </button>
@@ -190,7 +189,7 @@ function App() {
               className='hover-underline-animation__1'
               onClick={() => {
                 handleOpenModal('react');
-                track('React Span Click');
+                window.gtag('event', 'click', { 'event_category': 'span', 'event_label': 'React Span Click' });
               }}
               style={{ color: 'rgb(153 133 255)', cursor: 'pointer', textShadow: 'rgb(60 0 255) 0px 0px 12px' }}
             >
@@ -201,7 +200,7 @@ function App() {
               className='hover-underline-animation__2'
               onClick={() => {
                 handleOpenModal('ai');
-                track('AI Span Click');
+                window.gtag('event', 'click', { 'event_category': 'span', 'event_label': 'AI Span Click' });
               }}
               style={{ color: 'rgb(37 143 237)', cursor: 'pointer', textShadow: '0 0 12px #0071d5' }}
             >
@@ -212,7 +211,7 @@ function App() {
               className='hover-underline-animation__3'
               onClick={() => {
                 handleOpenModal('aws');
-                track('AWS Span Click');
+                window.gtag('event', 'click', { 'event_category': 'span', 'event_label': 'AWS Span Click' });
               }}
               style={{ color: '#ebb66a', cursor: 'pointer', textShadow: 'rgb(255 182 77) 0px 0px 12px' }}
             >
@@ -294,7 +293,7 @@ function App() {
         <div className='content'>
           <h3>WORKED AT</h3>
           <div className='exp'>
-            <span className='images' onClick={() => { handleOpenModal('farfetch'); track('Farfetch Logo Click'); }}>
+            <span className='images' onClick={() => { handleOpenModal('farfetch'); window.gtag('event', 'click', { 'event_category': 'logo', 'event_label': 'Farfetch Logo Click' }); }}>             
               <img src={farfetch} alt="farfetch" className="logo" />
               /
               <img src={sg} alt="stadium goods" className="logo" />
@@ -368,7 +367,7 @@ function App() {
               </div>
             </Modal>
 
-            <img src={eqx} alt="equinox" className="logo" onClick={() => { handleOpenModal('eqx'); track('Eqx Logo Click'); }} />
+            <img src={eqx} alt="equinox" className="logo" onClick={() => { handleOpenModal('eqx'); window.gtag('event', 'click', { 'event_category': 'logo', 'event_label': 'Eqx Logo Click' }); }} />            
             <Modal isOpen={isModalOpen.eqx} onAfterOpen={() => document.body.style.overflow = 'hidden'} onRequestClose={() => handleCloseModal('eqx')} style={customStyles} ariaHideApp={false}
               onAfterClose={() => document.body.style.overflow = 'auto'}
             >
@@ -416,8 +415,8 @@ function App() {
               </div>
             </Modal>
 
-            <img src={un} alt="un" className="un-logo" onClick={() => { handleOpenModal('un'); track('UN Logo Click'); }} />
-            <Modal isOpen={isModalOpen.un} onAfterOpen={() => document.body.style.overflow = 'hidden'} onRequestClose={() => handleCloseModal('un')} style={customStyles} ariaHideApp={false}
+            <img src={un} alt="un" className="un-logo" onClick={() => { handleOpenModal('un'); window.gtag('event', 'click', { 'event_category': 'logo', 'event_label': 'UN Logo Click' }); }} />           
+             <Modal isOpen={isModalOpen.un} onAfterOpen={() => document.body.style.overflow = 'hidden'} onRequestClose={() => handleCloseModal('un')} style={customStyles} ariaHideApp={false}
               onAfterClose={() => document.body.style.overflow = 'auto'}
             >
               <button style={customStyles.button} onClick={() => handleCloseModal('un')}>&times;</button>
@@ -456,7 +455,7 @@ function App() {
               </div>
             </Modal>
 
-            <img src={maxell} alt="maxell" className="logo" onClick={() => { handleOpenModal('maxell'); track('Maxell Logo Click'); }} />
+            <img src={maxell} alt="maxell" className="logo" onClick={() => { handleOpenModal('maxell'); window.gtag('event', 'click', { 'event_category': 'logo', 'event_label': 'Maxell Logo Click' }); }} />
             <Modal isOpen={isModalOpen.maxell} onAfterOpen={() => document.body.style.overflow = 'hidden'} onRequestClose={() => handleCloseModal('maxell')} style={customStyles} ariaHideApp={false}
               onAfterClose={() => document.body.style.overflow = 'auto'}
             >
